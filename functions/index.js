6a839319e769bd7bacd3f6a12aa57299c5fe0cc3
Chase Johnson <cjohnson19@pm.me>
2026-02-27 18:41:09 -0600

    refactor: remove more extra things

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,19 @@
-const { setGlobalOptions, logger } = require("firebase-functions");
+const { logger } = require("firebase-functions");
 const { onRequest } = require("firebase-functions/https");
-
 const express = require("express");
-const app = express();
 
-const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
-app.get("/", (req, res) => {
+const app = express();
+
+app.get("/", async (req, res) => {
+  const entries = (await db.collection("test-collection").get()).docs.map((e) =>
+    e.data(),
+  );
   logger.log("Inside app / handler");
-  res.send("hello, world");
+  res.send(`hello, ${JSON.stringify(entries)}`);
 });
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-exports.helloWorld = onRequest((request, response) => app(request, response));
+exports.handler = onRequest((request, response) => app(request, response));
